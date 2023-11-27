8d9bfaf00c972e29886e34713bab56328ded1b9a
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-11-27 18:34:26 +0100

    💄 update ui of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import img from "@/public/1.png"
 export default async function Home() {
   return (
     <>
-      <div className="absolute w-full h-full z-50 flex flex-col justify-center items-center px-5 gap-y-[3.75rem] lg:px-20 lg:items-start lg:gap-y-[5rem]">
+      <div className="absolute w-full h-full z-50 flex flex-col justify-center items-start px-5 gap-y-[3.75rem] lg:px-20 lg:gap-y-[5rem]">
         <p className="text-6xl font-gallery text-white lg:text-9xl lg:leading-[9.6rem]">meditation API</p>
         <p className="text-sm font-neue uppercase leading-[1.875] text-white lg:w-[25rem]">Welcome to our meditation API, a digital space crafted to enhance serenity and well-being. Explore our features with confidence, as our API is secure to ensure a peaceful and harmonious experience. Dive into guided meditation with assurance, as your peace of mind is our priority. Enjoy the fullness of this inner journey through our secure interface.
         </p>
